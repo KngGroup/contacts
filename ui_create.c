99c8a169e9334ae6b68e6dde729ce8a6c7ae932f
Dmitry Bykov <dmitry.bykov@sibers.com>
2016-05-24 17:02:10 +0600

    update create page

--- ui_create.c
+++ ui_create.c
@@ -1,26 +1,11 @@
 #include "ui_create.h"
 
-create_test_contacts() {
-    contacts_record_h contact;
-    contacts_record_h name;
-    contacts_record_h number;
-    int i;
-    contacts_connect_on_thread();
+Evas_Object *ui_create_page(Evas_Object *parent) {
+    
+}
 
-    for (i = 0; i < 10; i++) {
-        int id = -1;
-
-        contacts_record_create(_contacts_contact._uri, &contact);
-        contacts_record_create(_contacts_name._uri, &name);
-
-        contacts_record_set_str(name, _contacts_name.first, "John");
-        contacts_record_set_str(name, _contacts_name.last, "Smith");
-        contacts_record_add_child_record(contact, _contacts_contact.name, name);
-
-        contacts_record_create(_contacts_number._uri, &number);
-        contacts_record_set_str(number, _contacts_number.number, "+71111111111");
-        contacts_record_add_child_record(contact, _contacts_contact.number, number);
-
-        contacts_db_insert_record(contact, &id);
-    }
+void ui_show_create_page(appdata_s *ad) {
+    Evas_Object *page = ui_create_page(ad->win);
+    
+    elm_naviframe_item_push(ad->naviframe, "Create", NULL, NULL, page, NULL);
 }
